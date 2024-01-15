392ecd9b7dd6d35a624d28b77b0afcac3a4004c1
Jakub Mrozowski <kubamro13@gmail.com>
2024-01-15 23:27:55 +0100

    Making place better

--- darwinworld/src/main/java/agh/proj/model/interfaces/WorldMap.java
+++ darwinworld/src/main/java/agh/proj/model/interfaces/WorldMap.java
@@ -1,17 +1,16 @@
 package agh.proj.model.interfaces;
 
 import agh.proj.model.Animal;
-import agh.proj.model.MapDirection;
 import agh.proj.model.Vector2d;
 
 public interface WorldMap extends MoveValidator {
-    Vector2d moveValidator(WorldElement element, Vector2d newPosition);
+    Vector2d moveValidator(Animal animal, Vector2d newPosition);
 
     WorldElement objectAt(Vector2d position);
 
     void move(Animal animal);
 
-    void place(WorldElement element, Vector2d position);
+    void place(Animal animal, Vector2d position);
 
     boolean isOccupied(Vector2d position);
 
