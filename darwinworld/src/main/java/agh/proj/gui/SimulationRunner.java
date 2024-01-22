4576c4b50c77c9f87e06deb04a8a0bce7677883c
Jakub Mrozowski <kubamro13@gmail.com>
2024-01-22 15:33:38 +0100

    Check

--- darwinworld/src/main/java/agh/proj/gui/SimulationRunner.java
+++ darwinworld/src/main/java/agh/proj/gui/SimulationRunner.java
@@ -13,7 +13,6 @@ import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.geometry.HPos;
 import javafx.geometry.Insets;
-import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
